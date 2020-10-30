7cda91dca35b00cccff451c2a9a390ef863bd39f
jakobau <30506552+jakobau@users.noreply.github.com>
2020-10-29 21:56:01 -1000

    host sign in/out completed

--- Host/host.js
+++ Host/host.js
@@ -3,21 +3,48 @@
 * Version 1.0
 */
 
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+
+    // User is signed in.
+    console.log("signed in");
+    document.getElementById('login').style.display = 'none';
+	document.getElementById('dashboard').style.display = 'block';
+
+  } else {
+
+    // User is signed out.
+    console.log("signed out");
+    document.getElementById('login').style.display = 'block';
+	document.getElementById('dashboard').style.display = 'none';
+
+  }
+});
+
 var app = new Vue({
 	el: '#app',
 
 	data: {
+		email: null,
 	},
 
 	methods: {
-		dashboardPage: function() {
-			document.getElementById('login').style.display = 'none';
-			document.getElementById('dashboard').style.display = 'block';
+		login: function() {
+
+			firebase.auth().signInWithEmailAndPassword(email.value, password.value).catch(function(error) {
+			  // Handle Errors here.
+			  var errorCode = error.code;
+			  var errorMessage = error.message;
+
+			  console.log(errorCode);
+			  console.log(errorMessage);
+			  // ...
+			});
+
 		},
 
-		loginPage: function() {
-			document.getElementById('login').style.display = 'block';
-			document.getElementById('dashboard').style.display = 'none';
+		logout: function() {
+			firebase.auth().signOut();
 		}
 	}
 
